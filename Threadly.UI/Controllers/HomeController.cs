1013db3ea725344986864bb6995eefc97e4d57d0
mustafa altuntaş <2altuntas9@gmail.com>
2024-12-29 16:12:17 +0300

    .

--- Threadly.UI/Controllers/HomeController.cs
+++ Threadly.UI/Controllers/HomeController.cs
@@ -7,15 +7,12 @@ namespace Threadly.UI.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
+            if (TempData.ContainsKey("ToastifyMessage"))
+                ViewBag.ToastifyMessage = TempData["ToastifyMessage"];
+
             return View();
         }
 
@@ -31,10 +28,6 @@ namespace Threadly.UI.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+
     }
 }
