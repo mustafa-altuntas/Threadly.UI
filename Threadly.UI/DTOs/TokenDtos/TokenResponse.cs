b9848c48f91dcf08bb122b2c887820d822b16bc2
mustafa altuntaş <2altuntas9@gmail.com>
2024-12-29 16:14:09 +0300

    .

--- Threadly.UI/DTOs/TokenDtos/TokenResponse.cs
+++ Threadly.UI/DTOs/TokenDtos/TokenResponse.cs
@@ -1,7 +1,10 @@
-﻿namespace Threadly.UI.DTOs.TokenDtos
+﻿using Threadly.UI.Models.Tokens;
+
+namespace Threadly.UI.DTOs.TokenDtos
 {
     public class TokenResponse
     {
-
+        public Token AccessToken { get; set; }
+        public Token RefreshToken { get; set; }
     }
 }
